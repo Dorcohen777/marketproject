539410a30856e3187dfd9d94b596f8a0abe06852
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-12-18 20:42:50 +0200

    remove config from gitignore

---
+++ backend/config/dev.mjs
@@ -0,0 +1,5 @@
+export default  {
+  dbURL: 'mongodb+srv://surcvi:surcvi123@cluster0.kowiybv.mongodb.net/?retryWrites=true&w=majority',
+  dbName : 'market_db'
+  // dbURL: 'mongodb://127.0.0.1:27017',
+}
---
+++ backend/config/prod.mjs
@@ -0,0 +1,4 @@
+export default {
+  dbURL: 'mongodb+srv://surcvi:surcvi123@cluster0.kowiybv.mongodb.net/?retryWrites=true&w=majority',
+  dbName : 'market_db'
+}
